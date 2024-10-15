cea13f914663086e3009b1f71c14cde292c32d25
lolmageap <ekxk1234@naver.com>
2024-10-15 23:13:40 +0900

    project init

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+val kotlinVersion: String by project
+val logbackVersion: String by project
+
+plugins {
+    kotlin("jvm") version "2.0.21"
+    id("io.ktor.plugin") version "3.0.0"
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+}
+
+allprojects {
+    group = "extension.ktor"
+    version = "0.0.1"
+
+    repositories {
+        mavenCentral()
+    }
+}
+
+subprojects {
+    apply(plugin = "kotlin")
+
+    dependencies {
+        implementation("io.ktor:ktor-server-core-jvm")
+        testImplementation("io.ktor:ktor-server-test-host-jvm")
+        testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+    }
+}
