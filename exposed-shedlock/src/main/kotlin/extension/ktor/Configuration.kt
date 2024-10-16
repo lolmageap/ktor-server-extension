8c6d1fcd60761ea1df66e8f013f40dbed5807324
lolmageap <ekxk1234@naver.com>
2024-10-16 14:26:08 +0900

    implement shedlock module

--- exposed-shedlock/src/main/kotlin/extension/ktor/Configuration.kt
+++ exposed-shedlock/src/main/kotlin/extension/ktor/Configuration.kt
@@ -1,22 +1,29 @@
 package extension.ktor
 
 import io.ktor.server.application.*
-import org.jetbrains.exposed.sql.vendors.ForUpdateOption
+import org.jetbrains.exposed.sql.SchemaUtils
 
-// 이게 Spring 에서 AutoConfiguration 처럼 구현 하는 건데.... 참... 애매~~하다. 어카지 고민해 봐야 겠다...
-fun Application.module() {
+/**
+ * 1일차 - 이게 Spring 에서 AutoConfiguration 처럼 구현 하는 건데.... 참... 애매~~하다. 어카지 고민해 봐야 겠다...
+ *
+ * 2일차 -customizing 한 경로는 어떻게 처리 해야할 지 모르겠네 ㅠㅠ
+ * spring 에서는 @Configuration 이나 @ComponentScan 이런거로 처리 하지만... ktor 에서는 어떻게 해야할 지 모르겠다......
+ *
+ */
+fun Application.shedlockModule() {
     val driverToString =
-        environment.config.propertyOrNull("ktor.datasource.driverClassName")?.getString()
-            ?: environment.config.propertyOrNull("shedlock.datasource.driverClassName")?.getString()
-            ?: throw IllegalArgumentException("Driver class name is not found in the configuration file")
+        environment.config.propertyOrNull("ktor.datasource.driverClassName")?.getString()?.lowercase()
+            ?: environment.config.propertyOrNull("ktor.shedlock.driver")?.getString()?.lowercase()
 
-    // TODO : 여기 별도의 class 로 만들어서 인터페이스에 구현체로 만들어야함.
-    val driver: Any = when (driverToString) {
-        "org.h2.Driver" -> ForUpdateOption.ForUpdate
-        "org.postgresql.Driver" -> ForUpdateOption.PostgreSQL
-        "com.mysql.cj.jdbc.Driver" -> ForUpdateOption.MySQL
-        "oracle.jdbc.OracleDriver" -> ForUpdateOption.Oracle
-        "org.mariadb.jdbc.Driver" -> ForUpdateOption.MariaDB
-        else -> throw IllegalArgumentException("Unsupported driver class name")
+    val driver = when {
+        driverToString == null -> DBMS.ELSE
+        driverToString.contains("postgresql") -> DBMS.POSTGRESQL
+        driverToString.contains("mysql") -> DBMS.MYSQL
+        driverToString.contains("oracle") -> DBMS.ORACLE
+        driverToString.contains("mariadb") -> DBMS.MARIADB
+        else -> DBMS.ELSE
     }
+
+    DriverType.dbms = driver
+    SchemaUtils.create(Shedlocks)
 }
