d76099a52a5ed38e763fe6dfd20d8f6eb1936b2d
lolmageap <ekxk1234@naver.com>
2024-10-16 23:37:25 +0900

    create main module

---
+++ exposed-shedlock/src/main/kotlin/extension/ktor/Application.kt
@@ -0,0 +1,11 @@
+package extension.ktor
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    shedlockModule()
+}
