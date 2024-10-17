ca15955c87eabeafbd856206480619c7b1c1c724
lolmageap <ekxk1234@naver.com>
2024-10-17 22:41:54 +0900

    add shedlock test case

--- exposed-shedlock/src/test/kotlin/extension/ktor/ShedlockTest.kt
+++ exposed-shedlock/src/test/kotlin/extension/ktor/ShedlockTest.kt
@@ -1,41 +1,79 @@
 package extension.ktor
 
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.time.Duration
 
 class ShedlockTestStringSpec : StringSpec({
     beforeTest {
-        embeddedServer(Netty, port = 8080) {
-            shedlockModule()
-        }.start()
+        embeddedServer(Netty).start()
+
+        val dataSource = HikariDataSource(
+            HikariConfig().also {
+                it.jdbcUrl = "jdbc:postgresql://localhost:5432/shedlock"
+                it.username = "postgres"
+                it.password = "postgres"
+                it.validate()
+            }
+        )
+
+        Database.connect(dataSource)
+        transaction { SchemaUtils.create(Shedlocks) }
     }
 
     afterEach {
-        transaction {
-            SchemaUtils.drop(Shedlocks)
-        }
+        transaction { SchemaUtils.drop(Shedlocks) }
+        transaction { SchemaUtils.create(Shedlocks) }
     }
 
-    "여러개의 application이 동시에 실행 될 때 shedlock이 적용 되어 로직이 한번만 실행되는지 확인한다." {
+    afterTest {
+        transaction { SchemaUtils.drop(Shedlocks) }
+    }
+
+    "Verify that shedlock ensures the logic is executed only once when multiple applications start simultaneously with no existing data in the database" {
         val name = "test"
         val duration = 1.toMinutes()
         var count = 0
 
         coroutineScope {
             launch { runCatching { shedlock(name, duration) { count += 1 } } }
-            for (i in 0 until 100) { launch { runCatching { shedlock(name, duration) { count += 1 } } } }
-            repeat(100) { launch { runCatching { shedlock(name, duration) { count += 1 } } } }
+            repeat(50) { launch { runCatching { shedlock(name, duration) { count += 1 } } } }
+            repeat(50) { launch { runCatching { shedlock(name, duration) { count += 1 } } } }
+            repeat(50) { launch { runCatching { shedlock(name, duration) { count += 1 } } } }
+            repeat(50) { launch { runCatching { shedlock(name, duration) { count += 1 } } } }
         }
 
         count shouldBe 1
     }
+
+    "Verify that shedlock ensures the logic is executed only once when multiple applications start simultaneously with existing data in the database" {
+        val name = "test"
+        val duration = 1.toSeconds()
+        var count = 0
+
+        shedlock(name, duration) { count += 1 }
+        delay(1000)
+
+        coroutineScope {
+            repeat(50) { launch { runCatching { shedlock(name, duration) { count += 1 } } } }
+            repeat(50) { launch { runCatching { shedlock(name, duration) { count += 1 } } } }
+            repeat(50) { launch { runCatching { shedlock(name, duration) { count += 1 } } } }
+            repeat(50) { launch { runCatching { shedlock(name, duration) { count += 1 } } } }
+        }
+
+        count shouldBe 2
+    }
 })
 
 fun Int.toMinutes(): Duration = Duration.ofMinutes(this.toLong())
+fun Int.toSeconds(): Duration = Duration.ofSeconds(this.toLong())
