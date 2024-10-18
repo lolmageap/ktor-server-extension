a44efdbcdd589d963311caf9709592d318e12e98
lolmageap <ekxk1234@naver.com>
2024-10-18 15:23:56 +0900

    gradle setting for allprojects and subprojects

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-
 val kotlinVersion: String by project
 val logbackVersion: String by project
 
@@ -9,15 +7,9 @@ plugins {
     id("io.ktor.plugin") version "3.0.0"
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    withSourcesJar()
-    withJavadocJar()
-}
-
 allprojects {
-    group = "extension.ktor"
-    version = "0.0.1"
+    group = "com.github.lolmageap"
+    version = "0.0.3"
 
     repositories {
         mavenCentral()
@@ -26,28 +18,34 @@ allprojects {
 
 subprojects {
     apply(plugin = "kotlin")
-    apply(plugin = "com.github.johnrengelman.shadow")
+    apply(plugin = "maven-publish")
+
+    java {
+        sourceCompatibility = JavaVersion.VERSION_21
+        withSourcesJar()
+        withJavadocJar()
+    }
 
     dependencies {
         implementation("io.ktor:ktor-server-core-jvm")
         testImplementation("io.ktor:ktor-server-test-host-jvm")
         testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
     }
+
+    publishing {
+        publications {
+            create<MavenPublication>("mavenJava") {
+                groupId = project.group.toString()
+                artifactId = project.name
+                version = project.version.toString()
+                from(components["java"])
+            }
+        }
+    }
 }
 
 tasks {
     shadowJar {
         enabled = false
-    }
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            groupId = project.group.toString()
-            artifactId = project.name
-            version = project.version.toString()
-            from(components["java"])
-        }
     }
 }
