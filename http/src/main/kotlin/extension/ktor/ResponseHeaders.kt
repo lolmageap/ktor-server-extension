afa2f93f59009db9a00f5e9da8cfbe3dfb29315b
lolmageap <ekxk1234@naver.com>
2024-10-21 22:49:33 +0900

    remove response headers

--- http/src/main/kotlin/extension/ktor/ResponseHeaders.kt
+++
@@ -1,8 +0,0 @@
-package extension.ktor
-
-import io.ktor.server.response.*
-
-var ResponseHeaders.accessToken: String?
-    get() = get("Authorization")
-        ?: throw NoSuchElementException("access token is not found")
-    set(value) = append("Authorization", value ?: "")
