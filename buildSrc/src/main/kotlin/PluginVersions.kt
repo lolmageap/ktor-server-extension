98413e65fd03724031bb97581b34c7acc45d01bd
lolmageap <ekxk1234@naver.com>
2024-11-16 21:08:54 +0900

    create plugins

--- buildSrc/src/main/kotlin/PluginVersions.kt
+++ buildSrc/src/main/kotlin/PluginVersions.kt
@@ -1,4 +1,5 @@
 object PluginVersions {
     const val JVM_VERSION = "2.0.21"
     const val KTOR_PLUGIN_VERSION = "3.0.0"
+    const val SHADOW_JAR_VERSION = "8.1.1"
 }
---
+++ buildSrc/src/main/kotlin/Plugins.kt
@@ -0,0 +1,6 @@
+object Plugins {
+    const val JVM = "jvm"
+    const val SHADOW_JAR = "com.github.johnrengelman.shadow"
+    const val KTOR_PLUGIN = "io.ktor.plugin"
+    const val MAVEN_PUBLISH = "maven-publish"
+}
