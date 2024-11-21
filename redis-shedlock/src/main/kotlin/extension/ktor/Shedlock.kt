a71806b7695ab64943c8a4ab6aa63ebe31e70b7b
lolmageap <ekxk1234@naver.com>
2024-11-21 23:10:23 +0900

    temporary implement shedlock function

--- redis-shedlock/src/main/kotlin/extension/ktor/Shedlock.kt
+++ redis-shedlock/src/main/kotlin/extension/ktor/Shedlock.kt
@@ -1,19 +1,38 @@
 package extension.ktor
 
+import io.lettuce.core.RedisClient
+import org.redisson.Redisson
+import java.util.concurrent.TimeUnit.MILLISECONDS
 import kotlin.time.toJavaDuration
 
 suspend fun <T> shedlock(
     name: String,
     lockAtMostFor: kotlin.time.Duration,
+    resetLockUntilAfterComplete: Boolean = true,
     block: suspend () -> T,
 ) {
-    shedlock(name, lockAtMostFor.toJavaDuration(), block)
+    shedlock(name, lockAtMostFor.toJavaDuration(), resetLockUntilAfterComplete, block)
 }
 
+// TODO: 테스트 해야함
 suspend fun <T> shedlock(
     name: String,
     lockAtMostFor: java.time.Duration,
+    resetLockUntilAfterComplete: Boolean = true,
     block: suspend () -> T,
 ) {
-    block.invoke()
+    val redisAsyncCommands = RedisClient.create().connect().async()
+    val redisLock = Redisson.create().getLock(SHEDLOCK_PREFIX + name)
+
+    if (redisLock.isLocked) throw AlreadyLockedException()
+
+    redisLock.lock(lockAtMostFor.toMillis(), MILLISECONDS)
+
+    try {
+        block()
+    } finally {
+        if (resetLockUntilAfterComplete) redisLock.unlock()
+    }
 }
+
+private const val SHEDLOCK_PREFIX = "shedlock:"
