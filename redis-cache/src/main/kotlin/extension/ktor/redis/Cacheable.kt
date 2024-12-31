78d3a5e13a2d86061e2dff599e8b55573da6e490
lolmageap <ekxk1234@naver.com>
2024-12-31 22:23:51 +0900

    temporary implement cacheable scope function

---
+++ redis-cache/src/main/kotlin/extension/ktor/redis/Cacheable.kt
@@ -0,0 +1,33 @@
+package extension.ktor.redis
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.toJavaDuration
+
+// TODO : object mapper serialization and deserialization should be handled by the user
+suspend inline fun <reified T: Any> cacheable(
+    key: String,
+    ttl: Duration = 5.seconds,
+    cacheHit: () -> Unit = {},
+    cacheMiss: () -> Unit = {},
+    block: () -> T,
+): T {
+    val objectMapper = ObjectMapper()
+    val redisClient = RedissonClientHolder.redissonClient
+    val data = redisClient.getBucket<String>(key).get()
+
+    if (data != null) {
+        cacheHit.invoke()
+        return objectMapper.readValue(data, T::class.java) as T
+    } else {
+        cacheMiss.invoke()
+    }
+
+    return block().apply {
+        val serializedValue = objectMapper.writeValueAsString(this)
+        redisClient.getBucket<String>(key).set(serializedValue, ttl.toJavaDuration())
+    }
+}
+
+// TODO : Add cache locking mechanism
