07d364a12100e7a00e9e3c274d6f2da24efb8b68
lolmageap <ekxk1234@naver.com>
2025-01-09 23:59:14 +0900

    create cacheable test

--- ktor-extension-test/src/test/kotlin/cache/CacheableTest.kt
+++ ktor-extension-test/src/test/kotlin/cache/CacheableTest.kt
@@ -1,16 +1,24 @@
 package cache
 
 import extension.ktor.redis.RedissonClientHolder
+import extension.ktor.redis.cacheable
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.ints.shouldBeGreaterThan
+import io.kotest.matchers.shouldBe
 import io.lettuce.core.RedisClient
 import io.lettuce.core.RedisURI
 import io.lettuce.core.api.async.RedisAsyncCommands
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.redisson.Redisson
 import org.redisson.config.Config
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.time.Duration.Companion.seconds
 
 class CacheableTest : StringSpec({
     lateinit var redisClient: RedisAsyncCommands<String, String>
-    val database = mutableMapOf<String, String>()
 
     beforeTest {
         redisClient =
@@ -34,6 +42,28 @@ class CacheableTest : StringSpec({
     }
 
     "cacheable test" {
+        val key = "key"
+        database[key] = "value"
+        var number = 0
 
+        (0..10).map {
+            async {
+                val data =
+                    cacheable(key, 5.seconds) {
+                        number++
+                        database.find(key)
+                    }
+
+                database.getValue(key) shouldBe data
+            }
+        }.awaitAll()
+
+        number shouldBeGreaterThan 1
     }
 })
+
+private val database = mutableMapOf<String, String>()
+private suspend fun MutableMap<String, String>.find(key: String): String {
+    delay(1.seconds)
+    return this.getValue(key)
+}
