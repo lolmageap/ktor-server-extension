31b1d7999fca6a26c76f015fc911caddbc44a733
lolmageap <ekxk1234@naver.com>
2025-01-23 14:49:46 +0900

    implement rate limiter

--- server-protection/src/main/kotlin/extension/ktor/protection/RateLimiter.kt
+++ server-protection/src/main/kotlin/extension/ktor/protection/RateLimiter.kt
@@ -1,12 +1,40 @@
 package extension.ktor.protection
 
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import java.time.ZonedDateTime
+import kotlin.time.toKotlinDuration
+
+private val rateLimiterMap = mutableMapOf<String, RateLimiter>()
+private val mutex = Mutex()
+
+private data class RateLimiter(
+    val startUpTime: ZonedDateTime,
+    val count: Long,
+)
+
 suspend fun <T> rateLimiter(
     key: String,
     limit: Long,
     period: kotlin.time.Duration,
     block: suspend () -> T,
 ) {
-    TODO("Not implemented yet")
+    val now = ZonedDateTime.now()
+
+    mutex.withLock(RATE_LIMITER_PREFIX + key) {
+        if (rateLimiterMap hasNot key) rateLimiterMap[key] = RateLimiter(now, 1)
+
+        val rateLimiter = rateLimiterMap.getValue(key)
+
+        val isExpired = now - rateLimiter.startUpTime > period.inWholeMilliseconds
+        if (isExpired) rateLimiterMap[key] = RateLimiter(now, 1)
+
+        if (rateLimiter.count >= limit) {
+            rateLimiterMap[key] = rateLimiter.copy(count = rateLimiter.count + 1)
+            throw RateLimitExceededException()
+        }
+    }
+
     block()
 }
 
@@ -16,6 +44,31 @@ suspend fun <T> rateLimiter(
     period: java.time.Duration,
     block: suspend () -> T,
 ) {
-    TODO()
+    val now = ZonedDateTime.now()
+
+    mutex.withLock(RATE_LIMITER_PREFIX + key) {
+        if (rateLimiterMap hasNot key) rateLimiterMap[key] = RateLimiter(now, 1)
+
+        val rateLimiter = rateLimiterMap.getValue(key)
+
+        val isExpired = now - rateLimiter.startUpTime > period.toKotlinDuration().inWholeMilliseconds
+        if (isExpired) rateLimiterMap[key] = RateLimiter(now, 1)
+
+        if (rateLimiter.count >= limit) {
+            rateLimiterMap[key] = rateLimiter.copy(count = rateLimiter.count + 1)
+            throw RateLimitExceededException()
+        }
+    }
+
     block()
 }
+
+private const val RATE_LIMITER_PREFIX = "rate_limiter:"
+
+private infix fun <T, R> Map<T, R>.hasNot(
+    key: T,
+) = !containsKey(key)
+
+private operator fun ZonedDateTime.minus(
+    time: ZonedDateTime,
+) = this.toInstant().toEpochMilli() - time.toInstant().toEpochMilli()
