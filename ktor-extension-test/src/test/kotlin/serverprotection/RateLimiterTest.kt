3ecd57b250b89f91f908ba20940d9db0971312b2
lolmageap <ekxk1234@naver.com>
2025-01-24 23:48:28 +0900

    create rate limiter test

--- ktor-extension-test/src/test/kotlin/serverprotection/RateLimiterTest.kt
+++ ktor-extension-test/src/test/kotlin/serverprotection/RateLimiterTest.kt
@@ -1,9 +1,28 @@
 package serverprotection
 
+import extension.ktor.protection.rateLimiter
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.time.Duration.Companion.seconds
 
 class RateLimiterTest: StringSpec({
     "rate limiter test" {
-        TODO("Not implemented yet")
+        val key = "rate-limiter"
+        val number = AtomicInteger(0)
+
+        (1..50).map {
+            async {
+                runCatching {
+                    rateLimiter(key, 10, 1.seconds) {
+                        number.incrementAndGet()
+                    }
+                }
+            }
+        }.awaitAll()
+
+        number.get() shouldBe 10
     }
 })
